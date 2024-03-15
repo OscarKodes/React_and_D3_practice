5aee1548f9991a0ae7c4ef9a6b1605e8fcfd04bc
Oscar Ko <OscarKoEmail@gmail.com>
2024-03-15 10:45:00 -0400

    Add react based btn to click on the addmore btn

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,8 +2,13 @@ import React, { useState } from "react";
 import BarChart from "./BarChart";
 
 function App() {
+  function btnHandler() {
+    document.getElementById("btn").click();
+  }
+
   return (
     <div>
+      <button onClick={btnHandler}>Add More (React)</button>
       <BarChart />
     </div>
   );
