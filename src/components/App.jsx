9b9930bead9272678d39bfcebc737d6c483242f5
Oscar Ko <OscarKoEmail@gmail.com>
2024-03-16 12:16:20 -0400

    Do circle tutorial for react and d3. - Next figure out how to seperate it into components.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,56 @@
-import React, { useState } from "react";
-import BarChart from "./BarChart";
+import React, { useState, useRef, useEffect } from "react";
+import { select } from "d3";
 
 function App() {
-  function btnHandler() {
-    document.getElementById("btn").click();
-  }
+  const [data, setData] = useState([25, 30, 45, 60, 20]);
+  const svgRef = useRef();
+
+  const updateDataHandler = () => {
+    setData((prevState) => {
+      return prevState.map((d) => d + 5);
+    });
+  };
+
+  const filterDataHandler = () => {
+    setData((prevState) => {
+      return prevState.filter((d) => d <= 35);
+    });
+  };
+
+  const resetDataHandler = () => {
+    setData((prevState) => {
+      return prevState.filter((d) => d <= 35);
+    });
+  };
+
+  useEffect(() => {
+    console.log(svgRef);
+
+    const svg = select(svgRef.current);
+
+    svg
+      .selectAll("circle")
+      .data(data)
+      .join(
+        (enter) => enter.append("circle"),
+        (update) => update.attr("class", "updated"),
+        (exit) => exit.remove()
+      )
+      .attr("r", (d) => d)
+      .attr("cx", (d) => d * 2)
+      .attr("cy", (d) => d * 2)
+      .attr("fill", "green")
+      .attr("stroke", "red");
+  }, [data]);
 
   return (
-    <div>
-      <button onClick={btnHandler}>Add More (React)</button>
-      <BarChart />
-    </div>
+    <React.Fragment>
+      <svg ref={svgRef}></svg>
+      <br />
+      <button onClick={updateDataHandler}>Update Data</button>
+      <button onClick={filterDataHandler}>Filter Data</button>
+      <button onClick={resetDataHandler}>Reset Data</button>
+    </React.Fragment>
   );
 }
 
