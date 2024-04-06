88de9e6284775d355d9d6f04d4264f3f18c0c3c0
Oscar Ko <OscarKoEmail@gmail.com>
2024-04-06 09:18:38 -0400

    Worked on axis tutorial, but the axes do not show up.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,66 +1,68 @@
 import React, { useState, useRef, useEffect } from "react";
-import { select, line, curveCardinal } from "d3";
+import {
+  select,
+  line,
+  curveCardinal,
+  axisBottom,
+  axisRight,
+  scaleLinear,
+} from "d3";
 
 function App() {
   const [data, setData] = useState([25, 30, 45, 60, 20, 65, 75]);
   const svgRef = useRef();
 
-  const updateDataHandler = () => {
-    setData((prevState) => {
-      return prevState.map((d) => d + 5);
-    });
-  };
-
-  const filterDataHandler = () => {
-    setData((prevState) => {
-      return prevState.filter((d) => d <= 35);
-    });
-  };
-
-  const resetDataHandler = () => {
-    setData([25, 30, 45, 60, 20, 65, 75]);
-  };
-
-  // Will be called initially and on every data change
+  // will be called initially and on every data change
   useEffect(() => {
-    console.log(svgRef);
+    const svg = select(svgRef.current);
+    const xScale = scaleLinear()
+      .domain([0, data.length - 1])
+      .range([0, 300]);
 
-    const svg = select(svgRef.current).style("background-color", "lavender");
+    const yScale = scaleLinear().domain([0, 150]).range([150, 0]);
+
+    const xAxis = axisBottom(xScale)
+      .ticks(data.length)
+      .tickFormat((index) => index + 1);
+    svg.select(".x-axis").style("transform", "translateY(150px)").call(xAxis);
+
+    const yAxis = axisRight(yScale);
+    svg.select(".y-axis").style("transform", "translateX(300px)").call(yAxis);
+
+    // generates the "d" attribute of a path element
     const myLine = line()
-      .x((value, i) => i * 50)
-      .y((value) => value)
+      .x((value, index) => xScale(index))
+      .y(yScale)
       .curve(curveCardinal);
 
-    // svg
-    //   .selectAll("circle")
-    //   .data(data)
-    //   .join(
-    //     (enter) => enter.append("circle"),
-    //     (update) => update.attr("class", "updated"),
-    //     (exit) => exit.remove()
-    //   )
-    //   .attr("r", (d) => d)
-    //   .attr("cx", (d) => d * 2)
-    //   .attr("cy", (d) => d * 2)
-    //   .attr("fill", "green")
-    //   .attr("stroke", "red");
-
+    // renders path element, and attaches
+    // the "d" attribute from line generator above
     svg
-      .selectAll("path")
+      .selectAll(".line")
       .data([data])
       .join("path")
-      .attr("d", (value) => myLine(value))
+      .attr("class", "line")
+      .attr("d", myLine)
       .attr("fill", "none")
       .attr("stroke", "blue");
   }, [data]);
 
   return (
     <React.Fragment>
-      <svg ref={svgRef}></svg>
+      <svg ref={svgRef}>
+        <g className="x-axis" />
+        <g className="y-axis" />
+      </svg>
       <br />
-      <button onClick={updateDataHandler}>Update Data</button>
-      <button onClick={filterDataHandler}>Filter Data</button>
-      <button onClick={resetDataHandler}>Reset Data</button>
+      <br />
+      <br />
+      <br />
+      <button onClick={() => setData(data.map((value) => value + 5))}>
+        Update data
+      </button>
+      <button onClick={() => setData(data.filter((value) => value < 35))}>
+        Filter data
+      </button>
     </React.Fragment>
   );
 }
